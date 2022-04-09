0827e7e30f1f3831ff0c809e04addedf7e3cee93
valmarfina <valmarfina@gmail.com>
2022-04-09 23:24:26 +0300

    initial commit

---
+++ src/test/java/marfina/calc/CalcApplicationTests.java
@@ -0,0 +1,13 @@
+package marfina.calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
